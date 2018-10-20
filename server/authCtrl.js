045c592954a4b6c835d912a1f2a1e95b589165e7
John MacIsaac <johnmi@protonmail.com>
2018-10-20 18:07:30 -0500

    "created/updated backend files"

---
+++ server/authCtrl.js
@@ -0,0 +1,43 @@
+const Auth0Strategy = require("passport-auth0");
+
+const strategy = new Auth0Strategy({
+  domain:       process.env.DOMAIN,
+  clientID:     process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: '/login',
+  scope: "openid email profile"
+}, 
+(accessToken, refreshToken, extraParams, profile, done) => { 
+  // console.log(profile) 
+  // accessToken is the token to call Auth0 API (not nec in most cases)
+  // extraParams.id_token has the JSON Web Token
+  // profile has all the information from the user
+
+  // check db if user exists
+  // if y, return user 
+  // if n, add user to db, and then return user 
+
+  return done(null, profile);
+}); 
+
+const getUser = (req, res) => {
+  if(req.user) res.status(200).json(req.user);
+  else res.status(400).json({ message: 'Not logged in'})
+}
+
+const logout = (req, res) => {
+  req.session.destroy(() => {
+    /// 
+    res.redirect('http://localhost:3000/')
+    // res.redirect(process.env.REACT_APP_HOME);
+  });
+  
+}
+
+module.exports = {
+  strategy,
+  getUser,
+  logout
+};
+
+
--- server/index.js
+++ server/index.js
@@ -1,30 +1,87 @@
 require("dotenv").config();
 const express = require("express");
 const { json } = require("body-parser");
+const cors = require("cors");
 const massive = require("massive");
+const session = require("express-session");
+const passport = require('passport'); 
 
-// controllers
-const {  } = require("./ctrl");
+const { strategy, getUser, logout } = require(`${__dirname}/authCtrl`);
+
+const { test, saveInputs } = require("./calcCtrl");
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
-massive(process.env.CONNECTION_STRING).then(dbInstance => {
-  // console.log(process.env.CONNECTION_STRING); 
-  app.set("db", dbInstance);
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    // console.log(process.env.CONNECTION_STRING); // check
+    return app.set('db', db)
+  }) 
+  .catch(console.log);
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+    // cookie: { maxAge: 864000000 } /* 24 hours */
+    /* 1000ms * 60sec * 60min * 24hrs */
+  })
+);
+
+/* beg of Auth0 *************************************** */
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(strategy); 
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  const db = app.get('db'); 
+  db.getUserByAuthId([user.id])
+  .then(response => {
+    if (!response[0]) {
+      db.addUserByAuthId([
+        user.emails[0].value, 
+        user.displayName, 
+        user.id
+      ])
+      .then(res => {
+        done(null, res[0]);
+      })
+    } else {
+      return done(null, response[0]); 
+    } 
+  })
 });
 
-// test endpoint
-// app.get("/api/test", (req, res) => {
-//   res.status(200).json("Happy Success!!!!");
-// });
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+}); 
+
+app.get( '/login', 
+  passport.authenticate('auth0', { 
+    // successRedirect: '/me', 
+    successRedirect: 'http://localhost:3000/calculator', 
+    failureRedirect: '/login'
+    // failureFlash: true 
+  })
+);
+app.get('/me', getUser);
+app.get('/logout', logout);
+/* end of auth0 *************************************** */
+
+
 
 // endpoints
+app.get("/api/test", test); // test endpoint // good 
+app.post("/api/inputs", saveInputs);
+
 // app.get("/api/items", getItems);
 // app.delete("/api/item/:id", removeItem);
-// app.post("/api/item", addItem);
 // app.put("/api/item/:id", updatePrice);
 
-const port = 3001;
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
